a815ce7a0369fee2fce683cc13fa2f3d22ff3d29
Kenneth Gonzales <kenneth.gonzales@bicol-u.edu.ph>
2023-04-05 00:49:54 +0800

    updated script and user_interface
    added DOM and declined the use of inline onlick

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 function addToDisplay(num) {
     var display = document.getElementsByName("display")[0];
     display.value += num;
-  }
-  
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const numberButtons = document.querySelectorAll(".numberbutton");
+    numberButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            addToDisplay(button.value);
+        });
+    });
+
+    const acButton = document.querySelector("input[value='Ac']");
+    if (acButton) {
+        acButton.addEventListener("click", function () {
+            clearDisplay();
+        });
+    } else {
+        console.error("Element not found");
+    }
+
+    const delButton = document.querySelector("input[value='Del']");
+    if (delButton) {
+        delButton.addEventListener("click", function () {
+            deleteFromDisplay();
+        });
+    } else {
+        console.error("Element not found");
+    }
+
+    const arithmeticButtons = document.querySelectorAll(".arithmethic");
+    arithmeticButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            addToDisplay(button.value);
+        });
+    });
+
+    const equalsButton = document.querySelector(".equals");
+    if (equalsButton) {
+        equalsButton.addEventListener("click", function () {
+            calculate();
+        });
+    } else {
+        console.error("Element not found");
+    }
+
+    const decimalButton = document.querySelector("input[value='.']");
+    if (decimalButton) {
+        decimalButton.addEventListener("click", function () {
+            addDecimal();
+        });
+    } else {
+        console.error("Element not found");
+    }
+});
